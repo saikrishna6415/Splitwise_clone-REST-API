9c65e6702814d164157574c8886fa613fbe106c5
saikrishna <saikrishnakotagiri16@gmail.com>
2020-05-06 09:45:38 +0530

    config firestore database and create folder structure

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var admin = require("firebase-admin");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var serviceAccount = require('../database/database.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://splitwise-7d6e6.firebaseio.com"
 });
+
+const db = admin.firestore()
+
+
+// router.get('/', function (req, res, next) {
+//   db.collection('users').get()
+//     .then(snapshot => {
+//       var users = snapshot.docs.map(doc=> doc.data())
+//       console.log(users)
+//       res.status(200).send(users)
+//     });
+// });
 
 module.exports = router;
